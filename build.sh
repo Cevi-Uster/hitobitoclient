50d2b4191c02d775a3e0c0ec586c0b419ee8191a
Marc Baumgartner <marc@mabaka.ch>
2019-03-27 20:27:46 +0100

    Use maven-release-plugin for release builds.

--- build.sh
+++ build.sh
@@ -23,13 +23,18 @@ mv jdk-11.0.2+9-jre jre
 echo "Download JRE from AdoptOpenJDK for MacOS"
 wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.2_9.tar.gz
 tar -xvf OpenJDK11U-jre_x64_mac_hotspot_11.0.2_9.tar.gz
+mv jdk-11.0.2+9-jre jre_macos
 
 git clone https://github.com/Cevi-Uster/hitobitoclient.git
 cd hitobitoclient/cevi-db-client
 sed -i '' "s/@version@/${VERSION}/g" src/main/java/ch/cevi/db/client/configuration/Version.java
 sed -i '' "s/@releasedate@/${BUILDDATE}/g" src/main/java/ch/cevi/db/client/configuration/Version.java
 sed -i '' "s/@version@/${VERSION}/g" Setup.nsi
-mvn clean package appbundle:bundle  -Dmaven.test.skip=true
+
+#mvn clean package appbundle:bundle  -Dmaven.test.skip=true
+mvn release:prepare -Dmaven.test.skip=true
+mvn release:clean -Dmaven.test.skip=true
+
 
 makensis Setup.nsi
 mkdir -p $PROJECTDIR/release
